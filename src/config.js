9b8c5b552203ca721a2da6db2179e4eba9678b21
Killerpac <nilavasarkar@gmail.com>
2021-06-21 22:39:26 +0530

    bettere

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
 secret:{
     "TOKEN":process.env['TOKEN'],
     "CHANNEL": process.env['CHANNEL'],
-    "LINKS": "https://www.youtube.com/watch?v=jipGjSq4Plg,https://www.youtube.com/watch?v=VhomK8ABNP0,https://www.youtube.com/watch?v=tuXGeNTKsuo,https://www.youtube.com/watch?v=CerL_obhD9Y,https://www.youtube.com/watch?v=rJFYm_55wDU,https://www.youtube.com/watch?v=Wds5qlnNJJA,https://www.youtube.com/watch?v=buXtO72PtLg,https://www.youtube.com/watch?v=JIKZA7Tnex0,https://www.youtube.com/watch?v=t6JZnMNN6c8,https://www.youtube.com/watch?v=hp9eUYicGMw,https://www.youtube.com/watch?v=zzQ9KUeQ-_g"
+    "LINKS": "https://www.youtube.com/watch?v=jipGjSq4Plg,https://www.youtube.com/watch?v=Ma0hpqGzfrY,https://www.youtube.com/watch?v=tuXGeNTKsuo,https://www.youtube.com/watch?v=CerL_obhD9Y,https://www.youtube.com/watch?v=rJFYm_55wDU,https://www.youtube.com/watch?v=Wds5qlnNJJA,https://www.youtube.com/watch?v=buXtO72PtLg,https://www.youtube.com/watch?v=JIKZA7Tnex0,https://www.youtube.com/watch?v=t6JZnMNN6c8,https://www.youtube.com/watch?v=hp9eUYicGMw,https://www.youtube.com/watch?v=zzQ9KUeQ-_g"
   }
 }
