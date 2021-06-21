72fb7f305cddd07bce59eed33a081d677ab59b38
Killerpac <nilavasarkar@gmail.com>
2021-06-21 04:36:58 -0700

    at this point i dont know what i am doing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
-const { TOKEN, CHANNEL, LINKS} = require("./config.json");
+const CHANNEL= process.env['CHANNEL']
+
+const TOKEN= process.env['TOKEN'];
+
+const LINKS = require("./config.json")
 const { Client, Intents} = require("discord.js");
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES,Intents.FLAGS.GUILDS] });
 const ytdl = require('ytdl-core-discord');
