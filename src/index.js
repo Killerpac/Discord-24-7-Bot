5c4de6961f4ba052ac8f90d736c18b053b773d1d
Kale <kale@pop-os.localdomain>
2021-06-25 16:26:57 +0530

    fix permissions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { Client, Intents} = require("discord.js");
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES,Intents.FLAGS.GUILDS] });
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES,Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MEMBERS] });
 client.config = require('./config');
 const CHANNEL = client.config.secret.CHANNEL
 const TOKEN = client.config.secret.TOKEN
