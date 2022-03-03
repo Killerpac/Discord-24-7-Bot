672b5bcb9a838bdeaa74d9f38fc059623eae80e7
Killerpac <45239423+Killerpac@users.noreply.github.com>
2022-03-03 15:57:16 +0530

    revert

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var port = process.env.PORT || 8080;
 //create a server object:
-require('./nino')
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
